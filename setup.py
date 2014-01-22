ce0da8c5d7e6587a88322a2494d502e953cd8cd4
James Tiplady <biggleszx@biggleszx.com>
2014-01-22 14:18:56 +0000

    Moving requirements into setup.py; fleshing out setup.py

--- setup.py
+++ setup.py
@@ -10,15 +10,21 @@ setup(
     author='James Tiplady',
     url='http://github.com/BigglesZX/touchpaper',
     packages=find_packages(),
+    install_requires=['boto==2.23.0'],
+    entry_points={
+        'console_scripts': [
+            'touchpaper = touchpaper.main:main',
+        ]
+    },
     classifiers=[
-          'Development Status :: 1 - Planning',
-          'Environment :: Console',
-          'Intended Audience :: System Administrators',
-          'Operating System :: MacOS :: MacOS X',
-          'Operating System :: Unix',
-          'Operating System :: POSIX',
-          'Programming Language :: Python',
-          'Topic :: System :: Systems Administration',
-          'Topic :: Utilities',
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
     ],
 )
